782562639a916ee2e12e264f6d03debf3e94813c
Hugo S. Dias <hugo_sdias@hotmail.com>
2022-11-19 08:10:18 -0300

    ajustes na classe Livro

--- Models/Livro.cs
+++ Models/Livro.cs
@@ -5,8 +5,10 @@ using System.Threading.Tasks;
 
 namespace teste
 {
+    
     public class Livro
     {
+        static List<Livro> listLivros = new List<Livro>();
         public int Id { get; set; }
         public string Nome { get; set; }
         public string Autor { get; set; }
@@ -22,7 +24,6 @@ namespace teste
             this.Editora = editora;
         }
         
-
         public override string ToString()
         {
             string retorno = " ";
@@ -33,5 +34,113 @@ namespace teste
             retorno += "Editora: " + this.Editora;
             return retorno;
         }
+
+        public static void CadastrarLivro()
+        {
+            Console.Write("Nome do Livro: ");
+            string nomeLivro = Console.ReadLine().ToUpper();
+
+            Console.Write("Autor: ");
+            string autorLivro = Console.ReadLine().ToUpper();
+
+            Console.Write("Genero: ");
+            string generoLivro = Console.ReadLine().ToUpper();
+
+            Console.Write("Ano: ");
+            string anoLivro = Console.ReadLine().ToUpper();
+
+            Console.Write("Editora: ");
+            string editoraLivro = Console.ReadLine().ToUpper();
+
+            Livro novoLivro = new Livro(nome: nomeLivro, autor: autorLivro, genero: generoLivro, ano: anoLivro, editora: editoraLivro);
+
+            listLivros.Add(novoLivro);
+
+            Console.Write("Livro Cadastrado com Sucesso.");
+            Console.WriteLine();
+        }
+        public static void ListarLivros()
+        {
+            for (int i = 0; i < listLivros.Count; i++)
+            {
+                Livro livro = listLivros[i];
+                Console.Write($"#{i} -");
+                Console.WriteLine(livro);
+            }
+        }
+        public static void EditarLivro()
+        {
+            Console.WriteLine("EDITAR LIVRO!");
+            Console.Write("Digite o código do LIVRO: ");
+            int indiceLivro = int.Parse(Console.ReadLine());
+
+            if(listLivros.Contains(listLivros[indiceLivro]))
+            {
+               {                 
+                    string opcaoUsuario = OpcaoUsuarioEditarLivro();
+                        
+                    while (opcaoUsuario.ToUpper() != "x")
+                    {
+                        switch (opcaoUsuario)
+                        {
+                            case "1":
+                                Console.Write("Digite o NOME do Livro: ");
+                                listLivros[indiceLivro].Nome = Console.ReadLine().ToUpper();
+                                Console.WriteLine("Alteração concluída.");
+                                Console.WriteLine();
+                                break;
+                            case "2":
+                                Console.Write("Digite o AUTOR do Livro: ");
+                                listLivros[indiceLivro].Autor = Console.ReadLine().ToUpper();
+                                Console.WriteLine("Alteração concluída.");
+                                Console.WriteLine();
+                                break;
+                            case "3":
+                                Console.Write("Digite o GENERO do Livro: ");
+                                listLivros[indiceLivro].Genero = Console.ReadLine().ToUpper();
+                                Console.WriteLine("Alteração concluída.");
+                                Console.WriteLine();
+                                break;
+                            case "4":
+                                Console.Write("Digite o ANO do Livro: ");
+                                listLivros[indiceLivro].Ano = Console.ReadLine().ToUpper();
+                                Console.WriteLine("Alteração concluída.");
+                                Console.WriteLine();
+                                break;
+                            case "5":
+                                Console.WriteLine("Digite a EDITORA do Livro: ");
+                                listLivros[indiceLivro].Editora = Console.ReadLine().ToUpper();
+                                Console.WriteLine("Alteração concluída.");
+                                Console.WriteLine();
+                                break;
+                            case "X":
+                                return;
+                        }
+                        opcaoUsuario = OpcaoUsuarioEditarLivro();
+                    }                  
+                }
+            }
+        }
+
+        private static string OpcaoUsuarioEditarLivro()
+        {
+            Console.Write("Seleciona a opção abaixo: ");
+            Console.WriteLine();
+            Console.WriteLine("1 - Editar NOME do LIVRO");
+            Console.WriteLine("2 - Editar NOME do AUTOR");
+            Console.WriteLine("3 - Editar GENERO do LIVRO");
+            Console.WriteLine("4 - Editar ANO do LIVRO");
+            Console.WriteLine("5 - Editar EDITORA do LIVRO");
+            Console.WriteLine("X - Retornar ao menu principal");
+            Console.WriteLine();
+
+            string opcaoUsuario = Console.ReadLine().ToUpper();
+            Console.WriteLine();
+            return opcaoUsuario;
+        }
+
+               
     }
-} 
+
+    
+}
--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@
 {
     internal class Program
     {
-        static List<Livro> listLivros = new List<Livro>();
+        
         private static void Main(string[] args)
         {
             string opcaoUsuario = ObterOpcaoUsuario();
@@ -12,13 +12,13 @@
                 switch (opcaoUsuario)
                 {
                     case "1":
-                        CadastrarLivro();
+                        Livro.CadastrarLivro();
                         break;
                     case "2":
-                        ListarLivros();
+                        Livro.ListarLivros();
                         break;
                     case "3":
-                        EditarLivro();
+                        Livro.EditarLivro();
                         break;
                     case "C":
                         Console.Clear();
@@ -31,93 +31,6 @@
             }
         }
 
-        public static void CadastrarLivro()
-        {
-            Console.Write("Nome do Livro: ");
-            string nomeLivro = Console.ReadLine().ToUpper();
-
-            Console.Write("Autor: ");
-            string autorLivro = Console.ReadLine().ToUpper();
-
-            Console.Write("Genero: ");
-            string generoLivro = Console.ReadLine().ToUpper();
-
-            Console.Write("Ano: ");
-            string anoLivro = Console.ReadLine().ToUpper();
-
-            Console.Write("Editora: ");
-            string editoraLivro = Console.ReadLine().ToUpper();
-
-            Livro novoLivro = new Livro(nome: nomeLivro, autor: autorLivro, genero: generoLivro, ano: anoLivro, editora: editoraLivro);
-
-            listLivros.Add(novoLivro);
-
-            Console.Write("Livro Cadastrado com Sucesso.");
-            Console.WriteLine();
-        }
-        public static void ListarLivros()
-        {
-            for (int i = 0; i < listLivros.Count; i++)
-            {
-                Livro livro = listLivros[i];
-                Console.Write($"#{i} -");
-                Console.WriteLine(livro);
-            }
-        }
-        
-        public static void EditarLivro()
-        {
-            Console.WriteLine("EDITAR LIVRO!");
-            Console.Write("Digite o código do LIVRO: ");
-            int indiceLivro = int.Parse(Console.ReadLine());
-
-            if(listLivros.Contains(listLivros[indiceLivro]))
-            {
-               {                 
-                    string opcaoUsuario = OpcaoUsuarioEditarLivro();
-                        
-                    while (opcaoUsuario.ToUpper() != "x")
-                    {
-                        switch (opcaoUsuario)
-                        {
-                            case "1":
-                                Console.Write("Digite o NOME do Livro: ");
-                                listLivros[indiceLivro].Nome = Console.ReadLine().ToUpper();
-                                Console.WriteLine("Alteração concluída.");
-                                Console.WriteLine();
-                                break;
-                            case "2":
-                                Console.Write("Digite o AUTOR do Livro: ");
-                                listLivros[indiceLivro].Autor = Console.ReadLine().ToUpper();
-                                Console.WriteLine("Alteração concluída.");
-                                Console.WriteLine();
-                                break;
-                            case "3":
-                                Console.Write("Digite o GENERO do Livro: ");
-                                listLivros[indiceLivro].Genero = Console.ReadLine().ToUpper();
-                                Console.WriteLine("Alteração concluída.");
-                                Console.WriteLine();
-                                break;
-                            case "4":
-                                Console.Write("Digite o ANO do Livro: ");
-                                listLivros[indiceLivro].Ano = Console.ReadLine().ToUpper();
-                                Console.WriteLine("Alteração concluída.");
-                                Console.WriteLine();
-                                break;
-                            case "5":
-                                Console.WriteLine("Digite a EDITORA do Livro: ");
-                                listLivros[indiceLivro].Editora = Console.ReadLine().ToUpper();
-                                Console.WriteLine("Alteração concluída.");
-                                Console.WriteLine();
-                                break;
-                            case "X":
-                                return;
-                        }
-                        opcaoUsuario = OpcaoUsuarioEditarLivro();
-                    }                  
-                }
-            }
-        }
         private static string ObterOpcaoUsuario()
         {
             Console.WriteLine();
@@ -133,24 +46,7 @@
             string opcaoUsuario = Console.ReadLine().ToUpper();
             Console.WriteLine();
             return opcaoUsuario;
-        }
-
-        private static string OpcaoUsuarioEditarLivro()
-        {
-            Console.Write("Seleciona a opção abaixo: ");
-            Console.WriteLine();
-            Console.WriteLine("1 - Editar NOME do LIVRO");
-            Console.WriteLine("2 - Editar NOME do AUTOR");
-            Console.WriteLine("3 - Editar GENERO do LIVRO");
-            Console.WriteLine("4 - Editar ANO do LIVRO");
-            Console.WriteLine("5 - Editar EDITORA do LIVRO");
-            Console.WriteLine("X - Retornar ao menu principal");
-            Console.WriteLine();
-
-            string opcaoUsuario = Console.ReadLine().ToUpper();
-            Console.WriteLine();
-            return opcaoUsuario;
-        }
+        }       
     }
 }
 
